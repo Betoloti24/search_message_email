7a46776465d0f32c27a388b4c06182c15f158f54
BetolotiPanda <rgonzalez@pandaid.com>
2025-02-20 07:47:55 -0400

    feat: Validación del correo con la base de datos

--- Admin/views.py
+++ Admin/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Usuario 
 from .forms import CorreoForm
+from .models import Usuario
 
 def ingresar_correo(request):
     if request.method == 'POST':
         form = CorreoForm(request.POST)
         if form.is_valid():
             correo = form.cleaned_data['correo']
-            print(f'Correo ingresado: {correo}')  # Imprime el correo en la consola
-            return render(request, 'exito.html', {'correo': correo})
+            try:
+                # Consultar la base de datos por el correo
+                usuario = Usuario.objects.get(correo=correo)
+                return render(request, 'exito.html', {'correo': usuario.correo, 'contraseña': usuario.contraseña})
+            except Usuario.DoesNotExist:
+                # Si el correo no existe, mostrar mensaje de error
+                return render(request, 'ingresar_correo.html', {'form': form, 'error': 'El correo ingresado no existe.'})
+        else:
+            # Manejar errores de validación del formulario
+            return render(request, 'ingresar_correo.html', {'form': form})
     else:
         form = CorreoForm()
 
